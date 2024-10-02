afdb84a4f13b6cfcfed58696dd85aaf37ab09de2
QuesoParmesano <pecazon03@gmail.com>
2024-10-02 03:45:57 -0300

    base de datos correcta

---
+++ DB/Database.sql
@@ -0,0 +1,33 @@
+create database galletas;
+use galletas;
+
+create table camioneros(
+idCamionero int(11) auto_increment,
+nombreCompleto varchar(250),
+dni int(11),
+modelo varchar(255),
+patente varchar(75),
+primary key (idCamionero)
+);
+
+create table clientes(
+idCliente int(11) auto_increment,
+nombre varchar(250),
+apellido varchar(250),
+dni int(11),
+telefono varchar(25),
+primary key (idCliente)
+);
+
+create table pedidos(
+idPedido int(11) auto_increment,
+idCamionero int(11),
+idCliente int(11),
+fechaRegistro date,
+fechaEntrega date,
+estado varchar(125),
+peso varchar(125),
+primary key (idPedido),
+foreign key(idCamionero) references camioneros(idCamionero),
+foreign key(idCliente) references clientes(idCliente)
+);
